b7e2bb6cacf3a512307d2c7ba05daf66ea25c0c3
Brandon Portheine <bportheine@gmail.com>
2018-03-06 12:22:40 +0100

    navbar component

---
+++ poopscoop4two/src/components/navbar.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+
+class NavBar extends PureComponent {
+  render() {
+    return(
+      {/*<a className = "Link">{ this.props.content }</a>*/}
+      {/*<a className = "Link">{ this.props.content }</a>*/}
+      {/*<a className = "Link">{ this.props.content }</a>*/}
+      {/*<a className = "Link">{ this.props.content }</a>*/}
+      {/*<a className = "Link">{ this.props.content }</a>*/}
+    )
+  }
+}
+
+export default Title
