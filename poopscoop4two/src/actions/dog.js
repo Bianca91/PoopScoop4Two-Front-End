ef21d1e07019a8fbb9349221b7032de0d1c4a532
nsch1 <nick.schooneman1@gmail.com>
2018-03-07 12:25:35 +0100

    Add get dogbreed to mainpage and commented out likeDog actioncreator

--- poopscoop4two/src/actions/dog.js
+++ poopscoop4two/src/actions/dog.js
@@ -1,5 +1,6 @@
 import * as request from 'superagent'
 
+const apiBaseUrl = 'http://localhost:4001'
 const randomDogUrl = 'https://dog.ceo/api/breeds/image/random'
 
 export const getRandomDog = () => (dispatch) => {
@@ -11,3 +12,16 @@ export const getRandomDog = () => (dispatch) => {
     }))
     .catch(err => alert(err))
 }
+/*
+export const likeDog = (userId, breed) => (dispatch) => {
+  const updates = request
+    .get(`${apiBaseUrl}/users/${userId}`)
+    .then(response => {
+      const breedStats = Object.assign({}, response.body.breedStats)
+      console.log(breedStats)
+      return { ...breedStats, [breed]: breedStats[breed] ? breedStats[breed] + 1 : 1 }
+    })
+  
+  console.log(updates)
+}
+*/
--- poopscoop4two/src/containers/MainPage.js
+++ poopscoop4two/src/containers/MainPage.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import Dog from '../components/Dog'
 import Title from '../components/Title'
-import {getRandomDog} from '../actions/dog'
+import {getRandomDog, likeDog} from '../actions/dog'
 import { connect } from 'react-redux'
 import NavBar from '../components/Navbar'
 import './mainpage.css'
@@ -12,27 +12,32 @@ import {dislike as dislikeAction} from '../actions/dogActions'
 
 export class MainPage extends PureComponent {
   static propTypes = {
-    dog: PropTypes.string.isRequired
+    dog: PropTypes.object.isRequired
   }
 
   componentWillMount() {
     this.props.getRandomDog()
   }
 
-  handleDislikeButton = (event) => {
-    console.log(event)
-    //this.props.dislikeAction(event)
+  handleDislikeButton = () => {
+    this.props.getRandomDog()
   }
-  handleLikeButton = (event) => {
-    console.log(event)
-    //this.props.dislikeAction(event)
+  handleLikeButton = () => {
+    this.props.likeDog(1, this.getDogBreed(this.props.dog.image))
+  }
+
+  getDogBreed = (imageUrl) => {
+    return imageUrl.split('/')[5]
   }
 
   render() {
+    if (!this.props.dog.image) return null
+
     return (
       <div className="MainPage">
         <NavBar/>
         <Title content='PoopScoop4Two'/>
+        {console.log(this.getDogBreed(this.props.dog.image))}
         <Dog image={this.props.dog.image} />
         <button className='MainPageButton Dislike' onClick={this.handleDislikeButton}>"dislike"</button>
         <button className='MainPageButton Like' onClick={this.handleLikeButton}>"like"</button>
@@ -46,4 +51,4 @@ const mapStateToProps = (reduxState) => {
      dog: reduxState.dog
   }
 }
-export default connect(mapStateToProps, {getRandomDog})(MainPage)
+export default connect(mapStateToProps, {getRandomDog, likeDog})(MainPage)
