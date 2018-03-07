eb03b77d614ff85014f3acf4ee055f8d99e3f9b3
Brandon Portheine <bportheine@gmail.com>
2018-03-07 16:59:02 +0100

    favicon change

--- poopscoop4two/src/reducers/currentUser.js
+++ poopscoop4two/src/reducers/currentUser.js
@@ -1,10 +1,14 @@
 import {USER_LOGIN_SUCCESS} from '../actions/users'
+import {USER_LOGOUT_SUCCESS} from '../actions/users'
 
 export default function (state = null, action) {
 	switch (action.type) {
 		case USER_LOGIN_SUCCESS:
 			return action.payload
-
+    case 'UPDATE_BREED_STATS':
+      return action.payload
+		case USER_LOGOUT_SUCCESS:
+			return action.payload
 		default: return state
 	}
 }
