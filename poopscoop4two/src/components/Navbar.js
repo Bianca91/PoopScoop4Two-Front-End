a7969131311bac4c8056f0256184686f5cf7c76d
Bot13 <ruhtarduhd@gmail.com>
2018-03-07 14:55:25 +0100

    add logout action in users

--- poopscoop4two/src/components/Navbar.js
+++ poopscoop4two/src/components/Navbar.js
@@ -3,12 +3,12 @@ import './Navbar.css'
 
 class NavBar extends PureComponent {
   render() {
-    return( 
+    return(
       <ul className="NavBar">
         <li><a>Profile</a></li>
         <li><a>Pet Shop</a></li>
         <li><a>Dog Pound</a></li>
-        <li><a>Log Out</a></li>
+        <li className="logout"><a>Log Out</a></li>
         <li className="settings"><a>Settings</a></li>
       </ul>
       /*<a className = "Link">{ this.props.content }</a>*/
