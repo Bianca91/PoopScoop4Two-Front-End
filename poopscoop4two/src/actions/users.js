37df207e83f478e375da79de90fb984b800c55a9
oksmelnik <melnik.oksana@gmail.com>
2018-03-07 17:17:28 +0100

    Signup form

--- poopscoop4two/src/actions/users.js
+++ poopscoop4two/src/actions/users.js
@@ -3,6 +3,9 @@ import * as request from "superagent";
 
 export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
 export const USER_LOGIN_FAILED = "USER_LOGIN_FAILED";
+export const USER_SIGNUP_SUCCESS = "USER_SIGNUP_SUCCESS";
+export const USER_SIGNUP_FAILED = "USER_SIGNUP_FAILED";
+
 
 
 const baseUrl = "http://localhost:4001";
@@ -29,3 +32,25 @@ export const login = (email, password) => dispatch => {
       }
     });
 };
+
+export const signup = (email, password, name) => (dispatch) =>
+
+  request
+		.post(`${baseUrl}/users`)
+		.send({ email, password, name })
+		.then(result => {
+			dispatch({
+				type: USER_SIGNUP_SUCCESS
+			})
+		})
+		.catch(err => {
+			if (err.status === 400) {
+				dispatch({
+					type: USER_SIGNUP_FAILED,
+					payload: err.response.body.message || 'Unknown error'
+				})
+			}
+			else {
+				console.error(err)
+			}
+		})
--- poopscoop4two/src/reducers/index.js
+++ poopscoop4two/src/reducers/index.js
@@ -1,11 +1,13 @@
 import dog from "./dog";
 import currentUser from "./currentUser";
 import login from "./login";
-import user from './user'
+import user from './user';
+import signup from "./signup";
 
 export default {
   dog,
   user,
   currentUser,
-  login
+  login,
+  signup
 };
