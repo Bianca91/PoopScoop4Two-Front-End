2ea7ea9d94ba36fe81a5c0b64f43bb44ee840026
Brandon Portheine <bportheine@gmail.com>
2018-03-08 13:38:11 +0100

    matches action inprogress

--- poopscoop4two/src/components/MatchesList.js
+++ poopscoop4two/src/components/MatchesList.js
@@ -2,16 +2,23 @@ import React, { PureComponent } from 'react'
 import './MatchesList.css'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
+import {getMatches} from '../actions/getMatches'
+
 
 class MatchesList extends PureComponent {
 render() {
 
-  const allUsers = ['user1', 'user2', 'user3']
+  const allUsers = [
+    {name:'user1', email:'example1@email.com'},
+    {name:'user2', email:'example2@email.com'},
+    {name:'user3', email:'example3@email.com'}
+  ]
 
     return (
       <div>
         <ul className='Pound'>
-          {allUsers.map( user => <li><h1>{user}</h1></li>)}
+          {allUsers.map( user =>
+            <li><h1>{user.name} = {user.email}</h1></li>)}
         </ul>
       </div>
     )}
